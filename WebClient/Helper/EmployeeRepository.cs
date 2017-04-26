48e481753c2623cbacbbb5fcc791fa9a641a21ac
FREI Sven <sven.frei@axa-tech.com>
2017-04-26 10:24:48 +0200

    Developed WebClient

---
+++ WebClient/Helper/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClient.Helper
+{
+    using Models;
+    /// <summary>
+    /// Fake repository to emulate database
+    /// </summary>
+    public class EmployeeRepository
+    {
+        private static List<Employee> _employees = new List<Employee>()
+        {
+            new Employee(){Id = 1, Department = "WILC", Firstname = "Lou", Lastname="Bahnhof", Salary=11},
+            new Employee(){Id = 2, Department = "A", Firstname = "L", Lastname="B", Salary=69}
+        };
+
+        public static List<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public static Employee GetById(int id)
+        {
+            return _employees.Single(e => e.Id == id);
+        }
+    }
+}
---
+++ WebClient/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClient.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
